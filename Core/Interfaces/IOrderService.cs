6f4c56a8acb6cfccc0264190694280ef0921a9de
Visanu <mvisanu@gmail.com>
2022-10-11 08:41:50 -0500

    Add Order API :happy:

---
+++ Core/Interfaces/IOrderService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.OrderAggregate;
+
+namespace Core.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAdrress);
+        Task<IReadOnlyList<Order>> GetOrderForUserAsync(string buyerEmail);
+        Task<Order> GetOrderByIdAsync(int id, string buyerEmail);
+        Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync();
+    }
+}
