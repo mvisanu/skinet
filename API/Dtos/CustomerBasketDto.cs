c46603bb891d4808e588c940a721224aee46ba4c
Visanu <mvisanu@gmail.com>
2022-10-27 15:09:24 -0500

    Add Payments Controller

--- API/Dtos/CustomerBasketDto.cs
+++ API/Dtos/CustomerBasketDto.cs
@@ -9,5 +9,10 @@ namespace API.Dtos
         public string Id { get; set; }
        
         public List<BasketItemDto> Items { get; set; }
+
+        public int? DeliveryMethodId {get; set;}
+
+        public string ClientSecret { get; set; }
+        public string PaymentIntentId { get; set; }
     }
 }
